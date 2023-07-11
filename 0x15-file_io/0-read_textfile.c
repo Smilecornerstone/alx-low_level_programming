24e93838366c3e6273a1ab51630ad46af73929a3
Smilecornerstone <adelekekayodesamuel@gmail.com>
2023-07-11 06:47:33 -0700

    0x15-file_io

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * read_textfile - reads a text file and prints it to the POSIX standard output.
+ * read_textfile - reads a text file and prints it to standard output.
  * @filename: filename.
  * @letters: numbers of letters printed.
  *
